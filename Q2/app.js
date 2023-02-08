b44773b9fed1782e24bb8cc0e3f0c73e02770cbd
zahraka <zahrakarvan@gmail.com>
2023-02-08 19:38:23 +0330

    edit js file of question 2

--- Q2/app.js
+++ Q2/app.js
@@ -1,14 +1,14 @@
-`use strict`
-// const callback = function(elem ){
-//     elem.style.backgroundColor = ‘red’
-//     }
-//     elemCreator(‘div’ , callback)
-
-    const callback = function(params){
-        params.style.backgroundColor = 'red'
-    };
-    function elemCreator(er,callback){
-        const params = document.getElementById(er);
-        callback(params)
-    }
-    elemCreator('elem',callback)
+`use strict`;
+// const creator = function(element,func){
+function creator(element, func) {
+  element = document.createElement("element");
+  document.body.appendChild(element);
+  func(element);
+}
+function callBack(element) {
+  element.style.backgroundColor = "red";
+  element.style.width = "200px";
+  element.style.height = "300px";
+  element.style.padding = "20px 100px";
+}
+creator("div", callBack);
