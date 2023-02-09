4a5a1a8c2daa001b79c70b6852af0ce44d2b8839
zahraka <zahrakarvan@gmail.com>
2023-02-09 11:10:17 +0330

    edit

--- Q1/app.js
+++ Q1/app.js
@@ -1,10 +1,21 @@
 `use strict`;
-let user = { family: "bary", age: 20 };
-function func() {
-  return function () {
-    this.name = "Jack";
+// let user = { family: "bary", age: 20 };
+// function func() {
+//   return function () {
+//     this.name = "Jack";
+//     return this;
+//   }.bind(user);
+// }
+// let result = func();
+// console.log(result());
+
+const user = {};
+function func(key) {
+  return function (value) {
+    this.key = value;
     return this;
-  }.bind(user);
+  };
 }
-let result = func();
-console.log(result());
+const nameSetter = func("name");
+nameSetter.call(user, "Jack");
+console.log(user);
